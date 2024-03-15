5ef7873ad62eb5e7c88183c1b915a95d4b5ce4ae
Nicole Wade <nwade0916@gmail.com.com>
2024-03-14 23:27:37 -0500

    Studio 2

--- part2/studio/src/components/Button.jsx
+++ part2/studio/src/components/Button.jsx
@@ -1,8 +1,17 @@
-function Button() {
-   return;
- }
+import SaveButton from "./SaveButton";
+import ClickedButton from "./ClickedButton";
+
+function Button(props) {
+  const saveButton = props.saveButton;
+  if (saveButton) {
+    return <SaveButton />;
+  }
+  return <ClickedButton />;
+}
+
+export default Button;
+
  
- export default Button;
  
  //need to import SaveButton and ClickedButton
  //create conditional for these buttons
