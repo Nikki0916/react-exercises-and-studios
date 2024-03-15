bcbb20c28f62e1ed9448417e1e8249fc35e7b7a0
Nicole Wade <nwade0916@gmail.com.com>
2024-03-14 23:06:31 -0500

    Studio part one

---
+++ part1/Studio/src/components/Description.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import styles from './Description.module.css';
+
+const RecipeAuthor = () => {
+   let authorLink = "https://www.loveandlemons.com/";
+   let authorPhoto = "https://cdn.loveandlemons.com/wp-content/uploads/2023/01/IMG_41499-layers.jpg";
+   let authorName = "Jeanine";
+
+   return (
+      <div className = {styles.recipeAuthorBlock}>
+         <img src={authorPhoto} alt = "Jeanine holding her book" className={styles.imageUpdates} />
+         <div>
+            <h3>{authorName}</h3>
+            <a href={authorLink}>Love and Lemons</a> 
+         </div>
+      </div>
+   );
+}
+
+class RecipeDescription extends React.Component {
+
+   render() {
+      return (
+         <div> 
+            <div>
+               <h1>Homemade Pasta</h1>
+               <p>This recipe is easy and yummy homemade pasta</p>
+            </div>
+            <RecipeAuthor />
+         </div>
+      );
+   }
+}
+
+export default RecipeDescription;
---
+++ part1/Studio/src/components/Photos.jsx
@@ -0,0 +1,7 @@
+import styles from './Description.module.css';
+
+export default function RecipePhoto () {
+   return (
+      <img src="https://cdn.loveandlemons.com/wp-content/uploads/2020/04/homemade-pasta-580x706.jpg" alt="recipe photo" className = {styles.imageUpdates} />
+   );
+}
